ea62cbe8b139d8e9067a09872f9409abb1022efb
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-09 21:15:57 +0300

    Asdrftg

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -7,7 +7,7 @@ export const initMongoConnection = async () => {
     const pwd = getEnvVariable('MONGODB_PASSWORD');
     const url = getEnvVariable('MONGODB_URL');
     const db = getEnvVariable('MONGODB_DB');
-    await mongoose.createConnection(
+    await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
     console.log('Mongo connection successfully established!');
@@ -15,3 +15,29 @@ export const initMongoConnection = async () => {
     console.log(error);
   }
 };
+
+
+
+// import mongoose from 'mongoose';
+// import { getEnvVariable } from '../utils/env.js';
+
+// export const initMongoConnection = async () => {
+//   try {
+//     const user = getEnvVariable('MONGODB_USER');
+//     const pwd = getEnvVariable('MONGODB_PASSWORD');
+//     const url = getEnvVariable('MONGODB_URL');
+//     const db = getEnvVariable('MONGODB_DB');
+//     const mongoURI = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+
+//     await mongoose.connect(mongoURI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 30000 // Optional: Increase timeout to 30 seconds
+//     });
+
+//     console.log('Mongo connection successfully established!');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// };
+
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,5 +2,6 @@ import { ContactsCollection } from '../db/models/contacts.js';
 
 export const findContacts = async () => {
   const data = await ContactsCollection.find();
+  console.log(data);
   return data;
 };
