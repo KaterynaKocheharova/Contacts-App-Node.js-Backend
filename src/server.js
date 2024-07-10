950d7f9058224bcff0f292376ec8389a6473a627
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-10 19:39:00 +0300

    finish cleaning the code

--- src/server.js
+++ src/server.js
@@ -1,37 +1,16 @@
 import express from 'express';
-import pino from 'pino';
-import pinoMiddleware from 'pino-http';
-import cors from 'cors';
 import { getEnvVariable } from './utils/env.js';
+import { setUpMiddlewares } from './middlewares/middlewares.js';
 import { setUpControllers } from './controllers/controllers.js';
 
 export const setUpServer = () => {
   const app = express();
   const PORT = getEnvVariable('PORT', 3000);
 
-  // ===================== PINO
-
-  const logger = pino({
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-        ignore: 'req,res,hostname,pid',
-        messageFormat: '{msg}',
-      },
-    },
-  });
-
-  app.use(pinoMiddleware({ logger }));
-
-  // ==================== CORS
-
-  app.use(cors());
-
-  // =================== SETTING UP CONTROLLERS
-
+  setUpMiddlewares(app);
   setUpControllers(app);
+
+  const logger = setUpMiddlewares(app);
 
   // ================= PORT LISTENING
 
