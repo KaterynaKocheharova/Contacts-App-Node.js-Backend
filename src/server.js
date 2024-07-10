71ac484de35964fa6676069fdb352f8d84460add
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-10 19:23:27 +0300

    finish main part

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ export const setUpServer = () => {
   const app = express();
   const PORT = getEnvVariable('PORT', 3000);
 
-  //   PINO
+  // ===================== PINO
+
   const logger = pino({
     transport: {
       target: 'pino-pretty',
@@ -24,42 +25,51 @@ export const setUpServer = () => {
 
   app.use(pinoMiddleware({ logger }));
 
-  //   CORS
+  // ==================== CORS
+
   app.use(cors());
 
   //  ROUTE GET CONTACTS
 
   app.get('/contacts', async (req, res) => {
-    logger.info(req.url);
     const contacts = await findContacts();
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts',
       data: contacts,
     });
   });
 
+  // =================== ROUTE GET CONTACT BY ID
 
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await findContactById(contactId);
-    if (contact) {
-      res.status(200).json({
-        data: contact,
-      });
-    } else {
+
+    if (!contact) {
       res.status(404).json({
         message: 'No contact with such id found',
       });
+      return;
     }
-  });
 
-  //   PROCESSING NON-EXISTING PATHS
-  app.get('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}`,
+      data: contact,
     });
   });
 
-  //   PORT LISTENING
+  // ================= PROCESSING NON-EXISTING PATHS
+
+  app.get('*', (req, res) => {
+    res.status(404).json({
+      message: 'Contact not found!',
+    });
+  });
+
+  // ================= PORT LISTENING
+
   app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
   });
