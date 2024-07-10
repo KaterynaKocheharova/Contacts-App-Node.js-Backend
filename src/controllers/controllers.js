27a555cf347f44d483e51f5494ebcf279f5a1b5b
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-10 19:29:57 +0300

    put controllers in a separate file

---
+++ src/controllers/controllers.js
@@ -0,0 +1,44 @@
+import { findContacts, findContactById } from "../services/contacts.js";
+
+export const setUpControllers = (app) => {
+
+ //  ROUTE GET CONTACTS
+
+ app.get('/contacts', async (req, res) => {
+    const contacts = await findContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data: contacts,
+    });
+  });
+
+  // =================== ROUTE GET CONTACT BY ID
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await findContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'No contact with such id found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}`,
+      data: contact,
+    });
+  });
+
+    // ================= PROCESSING NON-EXISTING PATHS
+
+    app.get('*', (req, res) => {
+        res.status(404).json({
+          message: 'Contact not found!',
+        });
+      });
+
+};
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino';
 import pinoMiddleware from 'pino-http';
 import cors from 'cors';
 import { getEnvVariable } from './utils/env.js';
-import { findContacts, findContactById } from './services/contacts.js';
+import { setUpControllers } from './controllers/controllers.js';
 
 export const setUpServer = () => {
   const app = express();
@@ -29,44 +29,9 @@ export const setUpServer = () => {
 
   app.use(cors());
 
-  //  ROUTE GET CONTACTS
+  // =================== SETTING UP CONTROLLERS
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await findContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts',
-      data: contacts,
-    });
-  });
-
-  // =================== ROUTE GET CONTACT BY ID
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await findContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'No contact with such id found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}`,
-      data: contact,
-    });
-  });
-
-  // ================= PROCESSING NON-EXISTING PATHS
-
-  app.get('*', (req, res) => {
-    res.status(404).json({
-      message: 'Contact not found!',
-    });
-  });
+  setUpControllers(app);
 
   // ================= PORT LISTENING
 
