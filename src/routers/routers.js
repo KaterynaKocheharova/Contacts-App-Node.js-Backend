e0f4bede4263d8378f6005977cf1d83d5e5420f1
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-13 16:06:31 +0300

    extract routers

---
+++ src/routers/routers.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { findContacts, findContactById } from "../services/contacts.js";
+
+const router = Router();
+export default router;
+
+ router.get('/contacts', async (req, res) => {
+    const contacts = await findContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data: contacts,
+    });
+  });
+
+  router.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await findContactById(contactId);
+
+      if (contact === null) {
+        return res.status(404).send({
+          message: 'No contact with such id found',
+        });
+      }
+
+      res.status(200).send({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).send({
+        status: 500,
+        message: 'An error occurred while retrieving the contact',
+        error: error.message,
+      });
+    }
+  });
+
