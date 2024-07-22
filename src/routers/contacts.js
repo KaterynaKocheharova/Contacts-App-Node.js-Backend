b2585df74088106824242a68255a4a05daf9f835
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-22 15:05:15 +0300

    add validateBody controller to every route

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,16 +9,16 @@ import {
 } from '../controllers/contacts.js';
 import express from "express";
 import { validateBody } from '../middlewares/validateBody.js';
-import { contactsValidationSchema } from '../validation/contacts.js';
+import { createContactValidationSchema, updateContactValidationSchema } from '../validation/contacts.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get('/', ctrlWrapper(findContactsController));
 router.get('/:contactId', ctrlWrapper(findContactByIdController));
-router.post('/', jsonParser, validateBody(contactsValidationSchema), ctrlWrapper(createContactController));
+router.post('/', jsonParser, validateBody(createContactValidationSchema), ctrlWrapper(createContactController));
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
-router.put('/:contactId', jsonParser, ctrlWrapper(upsertContactController));
-router.patch('/:contactId', jsonParser, ctrlWrapper(patchContactController));
+router.put('/:contactId', jsonParser, validateBody(updateContactValidationSchema), ctrlWrapper(upsertContactController));
+router.patch('/:contactId', jsonParser, validateBody(updateContactValidationSchema), ctrlWrapper(patchContactController));
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export const contactsValidationSchema = Joi.object({
+export const createContactValidationSchema = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
     'string.base': 'Username should be a string',
     'string.min': 'Username should have at least {#limit} characters',
@@ -21,3 +21,26 @@ export const contactsValidationSchema = Joi.object({
   }),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
+
+export const updateContactValidationSchema = Joi.object({
+  name: Joi.string().min(3).max(30).messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(9).max(13).messages({
+    'string.base': 'Number should be a string',
+    'string.min': 'Number should have at least {#limit} characters',
+    'string.max': 'Number should have at most {#limit} characters',
+    'any.required': 'Number is required',
+  }),
+  email: Joi.string().email().messages({
+    'string.base': 'Email should be a string',
+  }),
+  isFavorite: Joi.boolean().messages({
+    'string.base': 'isFavorite should be a string',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
+
