03a77a2cabec8510f927b653845af9781a38f9cb
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-22 15:40:04 +0300

    add objectId validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,15 +10,16 @@ import {
 import express from "express";
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactValidationSchema, updateContactValidationSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get('/', ctrlWrapper(findContactsController));
-router.get('/:contactId', ctrlWrapper(findContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(findContactByIdController));
 router.post('/', jsonParser, validateBody(createContactValidationSchema), ctrlWrapper(createContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
-router.put('/:contactId', jsonParser, validateBody(updateContactValidationSchema), ctrlWrapper(upsertContactController));
-router.patch('/:contactId', jsonParser, validateBody(updateContactValidationSchema), ctrlWrapper(patchContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.put('/:contactId', jsonParser, isValidId, validateBody(updateContactValidationSchema), ctrlWrapper(upsertContactController));
+router.patch('/:contactId', jsonParser, isValidId, validateBody(updateContactValidationSchema), ctrlWrapper(patchContactController));
 
 export default router;
