04cef7263c6b7da95b08f0c1de91ab26a8c3dc98
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-11 12:17:38 +0300

    rename upload dir

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -6,8 +6,8 @@ import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants/index.js';
 
 export const saveFileToUploadDir = async (file) => {
   await fs.rename(
-    path.join(TEMP_UPLOAD_DIR, file.filename),
-    path.join(UPLOAD_DIR, file.filename),
+    path.join(TEMP_UPLOAD_DIR),
+    path.join(UPLOAD_DIR),
   );
 
   return `${env('APP_DOMAIN')}/uploads/${file.filename}`;
