ed4206fb15fd18959db004f5eee03c38fbca71e2
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-20 15:52:21 +0300

    add register docs

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,7 +24,7 @@ export const registerUserController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully registered user',
+    message: 'Successfully registered user!',
     data: createdUser,
   });
 };
